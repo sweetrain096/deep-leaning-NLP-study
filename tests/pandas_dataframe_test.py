40f8f640963866c854a744ebd739f85a52665994
sweetrain096 <gpfhddl09@gmail.com>
2019-06-27 17:50:23 +0900

    pandas

---
+++ tests/pandas_dataframe_test.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+data = [['A', 20], ['B', 29], ['c', 24], ['d', 26]]
+data = pd.DataFrame(data)
+
+print(data)
--- tests/pandas_test_series.py
+++ tests/pandas_test_series.py
@@ -9,3 +9,7 @@ ages2 = pd.Series([18, 21, 20, 16, 32, 22],
                   index=['a', 'b', 'c', 'd', 'e', 'f'],
                   name="ages2")
 print(ages2)
+
+class_name = {'국어' : 90,'영어' : 70,'수학' : 100,'과학' : 80}
+class_name = pd.Series(class_name)
+print(class_name,'\n')
